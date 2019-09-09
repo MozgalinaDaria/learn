8fb7f0f6a4b78626ed4fb2d4632c9b6620bd815d
Мозгалина Дарья Сергеевна <mds00@kontur>
2019-09-10 01:22:45 +0500

    Второй способ. Умножили двухзначное на 10, сократили запись

--- 4. Случайное трехзначное число/main.go
+++ 4. Случайное трехзначное число/main.go
@@ -4,12 +4,5 @@ import "../dexes/console"
 import "../dexes/rand"
 
 func main () {
-	console.Write("Рандомное трехзначное число = ", randomNumber())
-}
-
-func randomNumber() int {
-	var threeDigitNumber int
-
-	threeDigitNumber = rand.RandomInt(100, 999)
-	return threeDigitNumber - threeDigitNumber % 10
+	console.Write("Рандомное трехзначное число = ", rand.RandomInt(10, 99) * 10)
 }
