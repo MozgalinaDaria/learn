bd28d8594adad8e4d04b91fcbea7581c96989621
Мозгалина Дарья Сергеевна <mds00@kontur>
2019-09-11 22:56:29 +0500

    Написала отдельный bool-метод для определения четности числа

--- 5. Найди нечет/main.go
+++ 5. Найди нечет/main.go
@@ -4,25 +4,19 @@ import "../dexes/console"
 
 func main () {
 	var first, second int
-	var oddness bool
 
 	console.Writeln("Последовательно введите четное и нечетное число:")
 	first = console.ReadInt()
 	second = console.ReadInt()
 
-	oddness = isOdd(first)
-
-	if oddness {
+	if isOdd(first) {
 		console.Write("Нечетное число = ", first)
 		return
-}
-	console.Write("Нечетное число = ", second)
-
-	func isOdd(number int) bool {
-
-		return (number | 1) == number
 	}
 
+	console.Write("Нечетное число = ", second)
+}
 
-
+func isOdd(number int) bool {
+	return (number | 1) == number
 }
