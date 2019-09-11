c664f56a08c351ef9f1e19b71d871f41c3e633c7
Мозгалина Дарья Сергеевна <mds00@kontur>
2019-09-11 23:56:57 +0500

    Решила через hash-map

---
+++ 6. Количество дней в году/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "../dexes/console"
+
+func main () {
+	var year int
+
+	console.Writeln("Введите год")
+	year = console.ReadInt()
+
+	if isLeap(year) {
+		console.Writeln("Год ", year, " - високосный")
+		return
+	}
+
+	console.Writeln("Год ", year, " - невисокосный")
+}
+
+func isLeap(year int) bool {
+	var isDividedBy map[int]bool
+	isDividedBy = make(map[int]bool)
+
+	isDividedBy[4] = year % 4 == 0
+	isDividedBy[100] = year % 100 == 0
+	isDividedBy[400] = year % 400 == 0
+
+	return isDividedBy[400] || !isDividedBy[100] && isDividedBy[4]
+}
