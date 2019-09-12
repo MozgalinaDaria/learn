d00e3117a3de9d673414489ee320b19e9854db82
Мозгалина Дарья Сергеевна <mds00@kontur>
2019-09-12 22:07:27 +0500

    Решила вложенным условием

---
+++ 7. Найти максимальное число из трёх/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "../dexes/console"
+
+func main() {
+	var a, b, c int
+
+	console.Writeln("Введите 3 числа")
+	a = console.ReadInt()
+	b = console.ReadInt()
+	c = console.ReadInt()
+
+	console.Writeln("Наибольшее из 3 чисел = ", max(a, b, c))
+}
+
+func max(a, b, c int) int {
+	if a > b {
+		if a > c {
+			return a
+		}
+
+		return c
+	}
+
+	return b
+}
