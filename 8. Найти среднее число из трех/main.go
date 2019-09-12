4e3696b29a6c21813d725ea81f8c0788b7c46f2c
Мозгалина Дарья Сергеевна <mds00@kontur>
2019-09-12 23:26:43 +0500

    Готово

---
+++ 8. Найти среднее число из трех/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "../dexes/console"
+
+func main()  {
+	var a, b, c int
+
+	console.Writeln("Введите 3 числа")
+	a = console.ReadInt()
+	b = console.ReadInt()
+	c = console.ReadInt()
+
+	console.Writeln("Среднее число = ", average(a, b, c))
+}
+
+func average(a, b, c int) int {
+	if a < b && b < c || a > b && b > c {
+		return b
+	}
+
+	if a < c && c < b || a > c && c > b {
+		return c
+	}
+
+	return a
+}
