918afe77ee7a03dc7430a8571393e4ea1f73c124
Мозгалина Дарья Сергеевна <mds00@kontur>
2019-09-13 23:09:42 +0500

    Создан булевый метод проверки длин сторон

---
+++ 10. Проверка кратности числа/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "../dexes/console"
+
+func main() {
+	var a, b int
+
+	console.Writeln("Введите 2 числа")
+	a = console.ReadInt()
+	b = console.ReadInt()
+
+	if b == 0 {
+		console.Writeln("Делить на ноль нельзя! Начните заново")
+		return
+	}
+
+	if isMultiplicity(a, b) {
+		console.Writeln("Первое число кратно второму")
+		return
+	}
+
+	console.Writeln("Первое число не кратно второму")
+}
+
+func isMultiplicity(a, b int) bool {
+	return a % b == 0
+}
---
+++ 11. Существование треугольника/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "../dexes/console"
+
+func main() {
+	var a, b, c int
+
+	console.Writeln("Введите длины каждой из трех сторон треугольника")
+	a = console.ReadInt()
+	b = console.ReadInt()
+	c = console.ReadInt()
+
+	if CompareLength(a, b, c) && CompareLength(b, a, c) && CompareLength(c, a, b) {
+		console.Writeln("Треугольник существует")
+		return
+	}
+
+	console.Writeln("Треугольник не существует")
+}
+
+func CompareLength(a, b, c int) bool {
+	return (b + c) > a
+}
