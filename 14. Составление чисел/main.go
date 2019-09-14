08bc429d31d45938742dccf128bf68b39bf8b328
Мозгалина Дарья Сергеевна <mds00@kontur>
2019-09-14 23:08:50 +0500

    Сделала метод, вычленяющий цифры из числа

--- 14. Составление чисел/main.go
+++ 14. Составление чисел/main.go
@@ -1,3 +1,29 @@
 package main
 
 import "../dexes/console"
+
+func main () {
+	var number, figure3, figure2, figure1 int
+	console.Writeln("Введите любое трехзначное число")
+	number = console.ReadInt()
+
+	figure1, figure2, figure3 = GetFigure (number, figure1, figure2, figure3)
+
+	console.Writeln(figure1, figure2, figure3)
+	console.Writeln(figure1, figure3, figure2)
+	console.Writeln(figure2, figure1, figure3)
+	console.Writeln(figure2, figure3, figure1)
+	console.Writeln(figure3, figure1, figure2)
+	console.Writeln(figure3, figure2, figure1)
+}
+
+func GetFigure (number, figure1, figure2, figure3 int) (int, int, int) {
+	figure3 = number % 10
+	figure2 = (number % 100) / 10
+	figure1 = number / 100
+
+	return figure3, figure2, figure1
+}
+
+
+
