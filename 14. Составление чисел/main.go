a248b929fbc19a6a391d64b9c541745b17bf526b
Мозгалина Дарья Сергеевна <mds00@kontur>
2019-09-14 23:15:50 +0500

    Поправила входные параметры функции

--- 14. Составление чисел/main.go
+++ 14. Составление чисел/main.go
@@ -7,7 +7,7 @@ func main () {
 	console.Writeln("Введите любое трехзначное число")
 	number = console.ReadInt()
 
-	figure1, figure2, figure3 = GetFigure (number, figure1, figure2, figure3)
+	figure1, figure2, figure3 = GetFigure (number)
 
 	console.Writeln(figure1, figure2, figure3)
 	console.Writeln(figure1, figure3, figure2)
@@ -17,7 +17,8 @@ func main () {
 	console.Writeln(figure3, figure2, figure1)
 }
 
-func GetFigure (number, figure1, figure2, figure3 int) (int, int, int) {
+func GetFigure (number int) (int, int, int) {
+	var  figure1, figure2, figure3 int
 	figure3 = number % 10
 	figure2 = (number % 100) / 10
 	figure1 = number / 100
