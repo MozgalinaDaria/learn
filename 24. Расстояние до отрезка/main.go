87a6cbd4246bfe04885165d28c2013ed1038a02f
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-10-04 01:16:12 +0500

    Решена задача № 26 на цикл для 2 рандомных периодов

---
+++ 24. Расстояние до отрезка/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"../base/console"
+	"math"
+)
+
+const PointIsInSegment = 0
+const PointIsToTheLeftOfSegment = 1
+const PointIsToTheRightOfSegment = - 1
+
+func main() {
+	var xFirst, хSecond, yFirst, ySecond, x, y float64
+	var occasion int
+
+	xFirst = console.ReadFloat("Введите xFirst отрезка: ")
+	yFirst = console.ReadFloat("Введите yFirst отрезка: ")
+	хSecond = console.ReadFloat("Введите хSecond отрезка: ")
+	ySecond = console.ReadFloat("Введите ySecond отрезка: ")
+	x = console.ReadFloat("Введите х точки: ")
+	y = console.ReadFloat("Введите y точки: ")
+
+	occasion = IsInSegment(xFirst, хSecond, yFirst, ySecond, x, y)
+	console.Writeln(occasion)
+	console.Writeln("Расстояние от точки до отрезка составляет: ", GetDistance(occasion, xFirst, хSecond, yFirst, ySecond, x, y))
+}
+
+func IsInSegment(xFirst, хSecond, yFirst, ySecond, x, y float64) int {
+	if xFirst <= x && x <= хSecond || yFirst <= y && y <= ySecond {
+
+		return PointIsInSegment
+	}
+
+	if x < xFirst || y < ySecond {
+
+		return PointIsToTheLeftOfSegment
+	}
+
+	return PointIsToTheRightOfSegment
+}
+
+func GetDistance(a int, хFirst, хSecond, yFirst, ySecond, x, y float64) float64 {
+	switch a {
+	case PointIsInSegment:
+		return math.Sqrt((хFirst-x)*(хFirst-x) + (ySecond-y)*(ySecond-y))
+	case PointIsToTheLeftOfSegment:
+		return math.Sqrt((хFirst-x)*(хFirst-x) + (ySecond-y)*(ySecond-y))
+	default:
+		return math.Sqrt((хSecond-x)*(хSecond-x) + (yFirst-y)*(yFirst-y))
+	}
+}
---
+++ 26. Первые циклы v2.0/main.go
@@ -0,0 +1,75 @@
+package main
+
+import "../base/console"
+import "../base/rand"
+
+func main () {
+	var number int
+
+	number = GetTheRandomNumber()
+	console.Writeln("Сгенерированное число = ", number)
+
+	console.Writeln("Числа от 0 до ", number, ": ")
+	if number >= 0 {
+		FromZeroToNumberPositive(number)
+		console.Writeln()
+		console.Writeln("Числа от ", number, "до 0: ")
+		FromNumberToZeroPositive(number)
+		console.Writeln()
+		console.Writeln("Все четные числа от 0 до ", number, ": ")
+		EvenNumbersPositive(number)
+	} else {
+		FromZeroToNumberNegative(number)
+		console.Writeln()
+		console.Writeln("Числа от ", number, "до 0: ")
+		FromNumberToZeroNegative(number)
+		console.Writeln()
+		console.Writeln("Все четные числа от 0 до ", number, ": ")
+		EvenNumbersNegative(number)
+	}
+}
+
+func GetTheRandomNumber() int{
+	period := rand.RandomInt(0, 1)
+
+	if period == 0 {
+		return rand.RandomInt(-15, -5)
+	}
+	return rand.RandomInt(5, 15)
+}
+
+func FromZeroToNumberNegative(number int) {
+	for i := 0; i >= number; i-- {
+		console.Write(i, " ")
+	}
+}
+
+func FromZeroToNumberPositive(number int) {
+	for i := 0; i <= number; i++ {
+		console.Write(i, " ")
+	}
+}
+
+func FromNumberToZeroPositive(number int) {
+	for i := number; i >= 0; i-- {
+		console.Write(i, " ")
+	}
+}
+
+func FromNumberToZeroNegative(number int) {
+	for i := number; i <= 0; i++ {
+		console.Write(i, " ")
+	}
+}
+
+func EvenNumbersNegative(number int) {
+	for i := 0; i >= number; i = i - 2 {
+		console.Write(i, " ")
+	}
+}
+
+func EvenNumbersPositive(number int) {
+	for i := 0; i <= number; i = i + 2 {
+		console.Write(i, " ")
+	}
+}
