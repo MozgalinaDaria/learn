a45572cfc3a1c694f17739cb8ba3cec9390ac779
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-10-07 04:26:44 +0500

    Рефакторинг записи решения задачи № 27

--- 27. Вложенный цикл/main.go
+++ 27. Вложенный цикл/main.go
@@ -7,11 +7,12 @@ func main (){
 
 	number = console.ReadInt("Введите число: ")
 	for i:= 0; i < number; i++{
-		count = count + 1
+		count += 1
 
 		for j := i * number + 1; j <= number * count; j++{
 			console.Write(j, " ")
 		}
+
 		console.Writeln()
 	}
 }
