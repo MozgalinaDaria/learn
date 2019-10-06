7baa7c65544551c6c9a4dbf4eb5f4d7e7635d6cc
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-10-07 04:22:41 +0500

    Приборка

--- 27. Вложенный цикл/main.go
+++ 27. Вложенный цикл/main.go
@@ -6,10 +6,10 @@ func main (){
 	var number, count int
 
 	number = console.ReadInt("Введите число: ")
-	for i:= 0; i < number; i ++{
+	for i:= 0; i < number; i++{
 		count = count + 1
 
-		for j := i * number + 1 ; j <= number * count; j++{
+		for j := i * number + 1; j <= number * count; j++{
 			console.Write(j, " ")
 		}
 		console.Writeln()
