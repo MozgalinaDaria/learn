8468b70814b03fa79e7ab97f1cbf7ef4ac908a23
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-10-08 00:29:31 +0500

    Решена задача № 28

---
+++ 28. Ферма/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "../base/console"
+
+func main(){
+	var n int
+
+	n = console.ReadInt("Введите степень n: ")
+	if n == 1 {
+		console.Write("1 верблюд, 2 барана, 3 таракана")
+		return
+	}
+
+	if n == 2 {
+		console.Write("3 верблюда, 4 барана, 5 тараканов")
+		return
+	} else {
+		console.Write("Решений нет")
+	}
+}
