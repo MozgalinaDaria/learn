f2e34ad0d363ba467b37b533d4506540bb37a481
Mozgalina Daria <mozgalinadaria@gmail.com>
2019-10-08 19:41:32 +0500

    Решена задача № 29

---
+++ 29. Сумма чётных и нечётных цифр числа/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"../base/console"
+	"math"
+)
+
+func main()  {
+	var number, figure, sumOfEven, sumOfOdd int
+
+	number = console.ReadInt("Введите число: ")
+	for i:= 0; number != 0; i++{
+		figure = number % 10
+		number = number / 10
+		if figure % 2 == 0{
+			sumOfEven = sumOfEven + figure
+		} else {
+			sumOfOdd = sumOfOdd + figure
+		}
+	}
+	console.Writeln("Сумма чётных цифр числа = ", math.Abs(float64(sumOfEven)), ", сумма нечетных цифр числа = ", math.Abs(float64(sumOfOdd)))
+}
