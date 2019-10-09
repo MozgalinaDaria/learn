b2d2be469c6f5c7113ce27593dd04601c7141dd8
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-10-10 01:00:53 +0500

    Решена задача №  33

---
+++ 33. Числа Фибоначчи/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "../base/console"
+
+func main() {
+	var count, first, second, next int
+	count = console.ReadInt("Сколько чисел Фибоначчи отобразить? ")
+
+	if count == 0 {
+		console.Writeln("Ни одного числа Фибоначчи")
+		return
+	}
+	if count == 1 {
+		console.Writeln("Число Фибоначчи: 0")
+		return
+	}
+	if count == 2 {
+		console.Writeln("Числа Фибоначчи: 0, 1")
+		return
+	}
+	if count > 2 {
+		first = 0
+		second = 1
+		console.Write("Числа Фибоначчи: ", first, " ", second, " ")
+
+		for i:= 0; i <= count - 2; i++ {
+			next = first + second
+			console.Write(next, " ")
+			first = second
+			second = next
+		}
+	}
+}
