7b310a1d9332fa414e83a534f99aa03c808a88da
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-10-12 00:55:19 +0500

    Решена задача № 36

---
+++ 36. Кубы чисел/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"../base/console"
+)
+
+func main() {
+	var first, second float64
+
+	first = console.ReadFloat("Введите первое число: ")
+	second = console.ReadFloat("Введите второе число: ")
+	console.Writeln("Кубы чисел от ", first, "до ", second, " : ")
+
+	for i := first; i <= second; i++ {
+	console.Write(i * i * i, " ")
+	}
+}
