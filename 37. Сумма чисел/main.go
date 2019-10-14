0e0dda78bfb84dad9330e86c020158047f13f68c
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-10-15 00:32:05 +0500

    Все еще проблемы с вычислением времени в задаче № 37

--- 37. Сумма чисел/main.go
+++ 37. Сумма чисел/main.go
@@ -3,6 +3,7 @@ package main
 import (
 "../base/console"
 "../base/rand"
+	"time"
 )
 
 func main() {
@@ -11,14 +12,19 @@ func main() {
 	number := rand.RandomInt(50, 100)
 	console.Writeln("Рандомно было сгенерировано число: ", number)
 
+	var startTime = time.Now()
 	for i := 10; i <= number; i++ {
 
 		sumFromTenCycle = sumFromTenCycle + i
 	}
-	console.Writeln("Сумма всех чисел от 10 до ", number, " = ", sumFromTenCycle)
+	var endTime  =  time.Since(startTime)
 
+	console.Writeln("Посчитано в цикле. Сумма всех чисел от 10 до ", number, " = ", sumFromTenCycle, "; время обработки цикла = ", endTime.Nanoseconds())
+
+	startTime = time.Now()
 	amountOfNumbersInProgression = (number - 10 + 1)
 	sumFromTenProgression = (10 + number) * amountOfNumbersInProgression / 2
-	console.Writeln("Посчитано по формуле арифметической прогрессии. Сумма всех чисел от 10 до ", number, " = ", sumFromTenProgression)
+	endTime  =  time.Since(startTime)
+	console.Writeln("Посчитано по формуле арифметической прогрессии. Сумма всех чисел от 10 до ", number, " = ", sumFromTenProgression , "; время обработки цикла = ", endTime.Nanoseconds())
 }
 
