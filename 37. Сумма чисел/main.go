ee8ca2bd2a8c42acaa9f37a2dc7b21c139e72214
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-10-19 17:22:48 +0500

    В задаче 37 допилила определение времени работы цикла и формулы

--- 37. Сумма чисел/main.go
+++ 37. Сумма чисел/main.go
@@ -7,23 +7,39 @@ import (
 )
 
 func main() {
-	var sumFromTenCycle, sumFromTenProgression, amountOfNumbersInProgression int
+	var i, j, sumFromTenCycle, sumFromTenProgression, amountOfNumbersInProgression int
 
 	number := rand.RandomInt(50, 100)
 	console.Writeln("Рандомно было сгенерировано число: ", number)
 
-	var startTime = time.Now()
-	for i := 10; i <= number; i++ {
-		sumFromTenCycle += i
+	var startTimeCycle = time.Now()
+
+	for i = 0; i < 100000000; i++ {
+		for j = 10; j <= number; j++ {
+			sumFromTenCycle += j
+		}
 	}
-	var endTime  =  time.Since(startTime)
+	var endTimeCycle  =  time.Since(startTimeCycle)
 
-	console.Writeln("Посчитано в цикле. Сумма всех чисел от 10 до ", number, " = ", sumFromTenCycle, "; время обработки цикла = ", endTime.Nanoseconds())
+	console.Writeln("Посчитано в цикле. Сумма всех чисел от 10 до ", number, " = ", sumFromTenCycle / i,
+	"; время обработки цикла, сек = ", endTimeCycle.Seconds())
 
-	startTime = time.Now()
-	amountOfNumbersInProgression = (number - 10 + 1)
-	sumFromTenProgression = (10 + number) * amountOfNumbersInProgression / 2
-	endTime  =  time.Since(startTime)
-	console.Writeln("Посчитано по формуле арифметической прогрессии. Сумма всех чисел от 10 до ", number, " = ", sumFromTenProgression , "; время обработки цикла = ", endTime.Nanoseconds())
+	var startTime = time.Now()
+
+	for i := 0; i < 100000000; i++ {
+		amountOfNumbersInProgression = (number - 10 + 1)
+		sumFromTenProgression = (10 + number) * amountOfNumbersInProgression / 2
+	}
+
+	var endTimeFormula = time.Since(startTime)
+
+	console.Writeln("Посчитано по формуле арифметической прогрессии. Сумма всех чисел от 10 до ", number, " = ",
+	sumFromTenProgression, "; время обработки формулы, сек = ", endTimeFormula.Seconds())
+
+	if endTimeCycle > endTimeFormula  {
+		console.Writeln("Цикл выполнялся дольше арифметической прогрессии")
+	} else {
+		console.Writeln("Цикл выполнялся быстрее арифметической прогрессии")
+	}
 }
 
