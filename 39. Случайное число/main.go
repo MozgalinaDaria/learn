eab2b1678879ee7f65e87888c4e967dc2c251744
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-10-19 23:31:18 +0500

    Решена задача № 39

--- 39. Случайное число/main.go
+++ 39. Случайное число/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"../base/console"
+	"../base/rand"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var randomPeriod int
+	var randomnumber float64
+	var finalnumber float64
+
+	randomPeriod = rand.RandomInt(1, 3)
+	switch randomPeriod {
+	case 1:
+		randomnumber = float64(rand.RandomInt(-1000, -500))+ float64(rand.RandomInt(1, 100)) / 1000
+	case 2:
+		randomnumber = float64(rand.RandomInt(0, 100))+ float64(rand.RandomInt(1, 100)) / 1000
+	case 3:
+		randomnumber = float64(rand.RandomInt(300, 400))+ float64(rand.RandomInt(1, 100)) / 1000
+	}
+
+	finalnumber, _ = strconv.ParseFloat(fmt.Sprintf("%f", randomnumber), 3)
+
+	console.Writeln("Сгенерированное трехзначное число с тремя знаками после запятой: ", finalnumber)
+}
