6cf6d1344fe0fa12826e4b4ef656af6b42f10904
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-10-27 00:47:01 +0500

    Решена задача № 42

---
+++ 42. Спамер/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"../base/console"
+)
+
+func main() {
+	var key string
+	var value int
+	var numberOfLogins int
+
+	logins := make(map[string]int)
+
+	numberOfLogins = console.ReadInt("Введите общее количество логинов: ")
+
+	for i := 0; i < numberOfLogins; i++ {
+		logins[console.ReadString("Введите логин: ")]++
+	}
+
+	console.Writeln("Логины спамеров: ")
+
+	for key, value = range logins {
+		if value > 1 {
+		console.Write(key, " ")
+		}
+	}
+}
---
+++ base/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	var key string
+	var value int
+	var exists bool
+
+	// Создаём map
+	data := make(map[string]int)
+
+	// вписываем значение 5 в ключ test
+	data["test"] = 5
+	// получаем значение из ключа test
+	value = data["test"]
+	fmt.Println(value)
+	fmt.Println("----------------------------")
+
+	// второй вариант получения значения из ключа test, во вторую переменную кладётся True если ключ есть и False если ключа нет
+	value, exists = data["test"]
+	fmt.Println(value, exists)
+	fmt.Println("----------------------------")
+
+	// когда ключа нет...
+	value, exists = data["not exists"]
+	fmt.Println(value, exists)
+	fmt.Println("----------------------------")
+
+	// ключа всё ещё нет, даже после верхней проверки, когда мы обратились к map, мы туда ещё ничего не писали
+	value, exists = data["not exists"]
+	fmt.Println(value, exists)
+	fmt.Println("----------------------------")
+
+	// даже если ключа нет, то всё равно можно делать инкремент, по умолчанию там будет 0
+	data["not exists"]++
+	fmt.Println(data["not exists"])
+	fmt.Println("----------------------------")
+
+	// теперь ключ есть
+	value, exists = data["not exists"]
+	fmt.Println(value, exists)
+	fmt.Println("----------------------------")
+
+	// цикл по всем ключам map
+	for key, value = range data {
+		fmt.Println(key, "|", value)
+	}
+}
