4875c119864ba4caa75c85467d325487835f8045
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-10-27 15:27:06 +0500

    Решена задача № 43

---
+++ 43. Степень/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "../base/console"
+
+func main() {
+
+	var x []int // делимое
+
+	n := console.ReadInt("Введите степень: ")
+	m := console.ReadInt("Введите делитель: ")
+	y := console.ReadInt("Введите получившийся остаток от деления: ")
+
+	console.Writeln("Следующие числа могут быть делимыми в этом равенстве:")
+
+	for i := 0; i <= m - 1; i++ {
+		nToDegree := 1
+
+		for j := 0; j < n; j++{
+			nToDegree *= i
+		}
+
+		if nToDegree % m == y {
+		x = append(x, i)
+		}
+	}
+
+	if len(x) > 0 {
+		console.Write(x)
+	} else {
+		console.Writeln("-1")
+	}
+}
