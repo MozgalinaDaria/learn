54cae1a415051e3b2bae99ecfb54b09e81916436
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-11-08 21:31:19 +0500

    Решена задача № 48

---
+++ 48. Упрощённый динамический калькулятор/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"../base/cast"
+	"../base/console"
+)
+
+func main() {
+	var str string
+
+	s := ""
+	console.Writeln("Введите пример вида: ")
+	console.Writeln("1")
+	console.Writeln("+")
+	console.Writeln("1")
+	console.Writeln("=")
+
+	for s != "=" {
+		s = console.ReadString()
+		str = str + s
+	}
+	example := [] rune (str)
+
+	Calculator(example)
+}
+
+func Calculator(example []rune) {
+	var i, total int
+	var numberFirst []rune
+
+	length := len(example)
+
+	for i = 0; i < length; i++ {
+		if example [i] != '+' && example [i] != '-' {
+			numberFirst = append(numberFirst, example [i])
+		} else {
+			break
+		}
+	}
+
+	operation := example [i]
+	numberSecond := (example [i+1 : length-1])
+	console.Writeln(operation, '+')
+	switch operation {
+	case '+':
+		total = cast.RuneToInt(numberFirst) + cast.RuneToInt(numberSecond)
+		console.Writeln(total)
+	case '-':
+		total = cast.RuneToInt(numberFirst) - cast.RuneToInt(numberSecond)
+		console.Writeln(total)
+	}
+}
