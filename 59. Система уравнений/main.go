c811fe860e4eeac0938e5382bd6f7956996ab42d
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-12-13 02:49:48 +0500

    Решена задача № 60

--- 59. Система уравнений/main.go
+++ 59. Система уравнений/main.go
@@ -4,10 +4,11 @@ import "../base/console"
 
 func main() {
 	var sum int
+
 	for pens := 0; pens < 30; pens++ {
 		for pencils := 0; pencils < 30; pencils++ {
 			for rubber := 30 - pencils - pens; rubber >= 0; rubber-- {
-				sum = (pens*10) + (pencils*5) + (rubber*2)
+				sum = (pens * 10) + (pencils * 5) + (rubber * 2)
 				if sum == 100 {
 					console.Writeln("Можно приобрести ручек, карандашей и ластиков в количестве ", pens, ", ",
 						pencils, ", ", rubber, "шт. соответственно")
---
+++ 60. Гипотеза Сиракуз/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "../base/console"
+
+const IsOdd int = 1
+const IsEven int = 2
+
+func main() {
+	number := console.ReadInt("Введите число для проверки гипотезы Сиракуз: ")
+	SyracuseHypothesis(number)
+}
+
+func SyracuseHypothesis(number int) {
+	for number != 1 {
+		switch CheckParity(number) {
+		case IsEven:
+			number /= 2
+			console.Write("|", number, "|", " ")
+		case IsOdd:
+			number = (number*3 + 1) / 2
+			console.Write("|", number, "|", " ")
+		}
+
+	}
+}
+
+func CheckParity(a int) int {
+	if a%2 == 0 {
+		return 2
+	}
+	return 1
+}
