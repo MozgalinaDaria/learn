5d6ce5e2709fbed6bd2fa97b30d2dc150a76c0ea
Мозгалина Дарья Сергеевна <mozgalinadaria@gmail.com>
2019-12-17 02:11:46 +0500

    Решена задача № 61

---
+++ 61. Таблица символов/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "../base/console"
+
+func main() {
+	number := 0
+
+	for line := 0; line <= 50; line++ {
+		for j := 0; j < 5; j++ {
+			console.Write("|", number, " ", string(number), "|")
+			number++
+		}
+		console.Writeln()
+
+	}
+	console.Writeln(number, " ", string(number))
+}
